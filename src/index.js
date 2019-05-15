c50ac65170d8fb037bef6f566fd2ca6ca8bc50ab
borisc1996 <borisc1996@gmail.com>
2019-05-15 18:05:26 +0100

    add endpoints for /users and /users/:id

--- src/index.js
+++ src/index.js
@@ -21,6 +21,29 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id).then((user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+    
+  }).catch((error) => {
+      return res.status(500).send();
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users);
+  }).catch((error) => {
+
+  });
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
 
