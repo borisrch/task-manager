e0e969d38caa6f90f319eaed5b140e5cf422f6c3
borisc1996 <borisc1996@gmail.com>
2019-05-15 17:45:35 +0100

    add seperate file to model task object.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,18 +3,4 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
-})
-
-// Mongoose pluralizes the model name, i.e. task --> tasks.
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  }
 });
