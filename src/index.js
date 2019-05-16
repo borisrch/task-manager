e195eb58e88064420a2a5d44aca85a319f796645
borisc1996 <borisc1996@gmail.com>
2019-05-16 18:03:44 +0100

    :sparkles: add DELETE /users/:id endpoint to delete user by id

--- src/index.js
+++ src/index.js
@@ -65,6 +65,18 @@ app.patch('/users/:id', async (req, res) => {
   } catch (e) {
     res.status(400).send(e);
   }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
 })
 
 app.post('/tasks', async (req, res) => {
