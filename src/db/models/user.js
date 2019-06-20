c5a760cc15abf30319f45601eec9a875d63c6ad3
borisc1996 <borisc1996@gmail.com>
2019-06-20 15:18:58 -0400

    add middleware for remove() to cascade delete tasks

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -3,6 +3,8 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Task = require('./task');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -103,6 +105,13 @@ userSchema.pre('save', async function(next) {
 
   next();
 });
+
+// Delete user tasks when user is removed.
+userSchema.pre('remove', async function(next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  next();
+})
 
 const User = mongoose.model('User', userSchema);
 
