1ea006671bee7808e0be1b098325f67d1bf748b5
borisc1996 <borisc1996@gmail.com>
2019-07-10 17:26:00 +0100

    add POST /users/me/avatar to accept images.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const User = require('../db/models/user');
 const auth = require('../middleware/auth');
 const express = require('express');
+const multer = require('multer');
 
 const router = new express.Router();
 
@@ -98,6 +99,25 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
+});
+
+const upload = new multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image.'));
+    }
+    cb(undefined, true);
+  }
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send();
+},  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
 });
 
 module.exports = router;
